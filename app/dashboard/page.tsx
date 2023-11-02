2a97c525adbafc2d914d6ae8266b4f7f8a998b4e
marcbrown2024 <marcbrown2024@gmail.com>
2023-11-02 15:03:56 -0400

    commit sixth --fourth

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,12 +9,27 @@ import { globalSideBar } from "../../globalStates/globalSideBar";
 
 //components
 import { useAuth } from "@/context/AuthContext";
+import BarChartBox from "@/components/barChartBox/BarChartBox";
+import BigChartBox from "@/components/bigChartBox/BigChartBox";
+// import ChartBox from "@/components/chartBox/ChartBox";
+import PieChartBox from "@/components/pieCartBox/PieChartBox";
+import TopBox from "@/components/topBox/TopBox";
 
 // assets
 import { StudentData } from "@/app/student/studentData";
+import {
+  barChartBoxRevenue,
+  barChartBoxVisit,
+  chartBoxConversion,
+  chartBoxProduct,
+  chartBoxRevenue,
+  chartBoxUser,
+} from "@/data";
 
-type Props = {
-};
+// css
+import "./home.scss";
+
+type Props = {};
 
 const Dashboard = (props: Props) => {
   const { isSidebarOpen, isSidebarHidden } = globalSideBar();
@@ -39,7 +54,35 @@ const Dashboard = (props: Props) => {
             "w-screen lg:w-[calc(100%-5rem)]"
       } flex items-center justify-center gap-8 transition-width duration-500`}
     >
-      <div className="h-full w-1/5 flex flex-col items-center justify-center"></div>
+      <div className="home">
+        <div className="box box1">
+          <TopBox />
+        </div>
+        <div className="box box2">
+          {/* <ChartBox {...chartBoxUser} /> */}
+        </div>
+        <div className="box box3">
+          {/* <ChartBox {...chartBoxProduct} /> */}
+        </div>
+        <div className="box box4">
+          <PieChartBox />
+        </div>
+        <div className="box box5">
+          {/* <ChartBox {...chartBoxConversion} /> */}
+        </div>
+        <div className="box box6">
+          {/* <ChartBox {...chartBoxRevenue} /> */}
+        </div>
+        <div className="box box7">
+          <BigChartBox />
+        </div>
+        <div className="box box8">
+          <BarChartBox {...barChartBoxVisit} />
+        </div>
+        <div className="box box9">
+          <BarChartBox {...barChartBoxRevenue} />
+        </div>
+      </div>
     </div>
   );
 };
