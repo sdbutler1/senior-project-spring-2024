e163840cd35c02308539e9538a482890b6ec240c
marcbrown2024 <marcbrown2024@gmail.com>
2023-11-04 13:53:31 -0400

    addition to eight commit

--- components/global/topbar/TopBar.tsx
+++ components/global/topbar/TopBar.tsx
@@ -8,7 +8,6 @@ import Link from "next/link";
 
 // components
 import { useAuth } from "@/context/AuthContext";
-import BubbleCount from "@/components/global/PopUpNotifications/BubbleCount";
 
 // global states
 import { globalSideBar } from "@/globalStates/globalSideBar";
@@ -19,14 +18,10 @@ import logo from "@/public/assets/shaw.png";
 
 // Icons
 import {
-  HiOutlineMail,
-  HiOutlineBell,
   HiOutlineUserCircle,
   HiMenu,
-  HiSearch,
 } from "react-icons/hi";
 import { GoTriangleDown } from "react-icons/go";
-import { BsSearch } from "react-icons/bs";
 
 type Props = {};
 
@@ -37,17 +32,11 @@ const Topbar = (props: Props) => {
     globalSideBar();
   const {
     isPopUpOpen1,
-    isPopUpOpen2,
-    isPopUpOpen3,
     setPopUpOpen1,
-    setPopUpOpen2,
-    setPopUpOpen3,
   } = usePopUpStore();
 
   const togglePopUp = (popupNumber: number) => {
     setPopUpOpen1(popupNumber === 1);
-    setPopUpOpen2(popupNumber === 2);
-    setPopUpOpen3(popupNumber === 3);
   };
 
   useEffect(() => {
@@ -55,8 +44,6 @@ const Topbar = (props: Props) => {
       const target = event.target as HTMLElement;
       if (!target.closest(".topbarPopup") && !target.closest(".popup")) {
         setPopUpOpen1(false);
-        setPopUpOpen2(false);
-        setPopUpOpen3(false);
       }
     };
     document.addEventListener("click", closePopupsOnOutsideClick);
@@ -65,11 +52,7 @@ const Topbar = (props: Props) => {
     };
   }, [
     isPopUpOpen1,
-    isPopUpOpen2,
-    isPopUpOpen3,
     setPopUpOpen1,
-    setPopUpOpen2,
-    setPopUpOpen3,
   ]);
 
   if (currentPathname === "/login" || currentPathname === "/forgotPassword") {
@@ -88,80 +71,17 @@ const Topbar = (props: Props) => {
           priority
         />
       </Link>
-      <form className="h-full w-3/6 invisible flex items-center justify-center">
-        <label className="h-4/6 w-3/6 flex items-center justify-start text-lg px-8 gap-2 rounded-3xl border border-[#7d1f2f90]">
-          <div className="h-full w-8 flex items-center justify-start text-xl text-[#7d1f2e]">
-            <BsSearch />
-          </div>
-          <input
-            type="text"
-            id="search"
-            name="search"
-            autoComplete="off"
-            placeholder="Search..."
-            className="w-full bg-transparent outline-none focus:border-b border-slate-500 p-2"
-          />
-        </label>
-      </form>
+
       <div className="h-full w-auto flex items-center justify-end gap-4 lg:gap-10 pr-2 lg:pr-8">
-        <button
-          type="button"
-          className="relative flex items-center justify-center"
-        >
-          <div className="flex flex-col items-center justify-center">
-            <li className="h-full w-full flex items-center justify-center">
-              <HiSearch className="text-[1.6rem] lg:text-[2rem] text-[#7d1f2e]" />
-            </li>
-          </div>
-        </button>
         <button
           type="button"
           onClick={() => togglePopUp(1)}
           className="relative flex items-center justify-center"
         >
-          <button className="absolute right-[1.8rem] h-auto w-full flex items-center justify-end ">
-            <GoTriangleDown
-              className={`text-2xl text-[#7d1f2e] ${
-                isPopUpOpen1 ? "flex" : "hidden"
-              }`}
-            />
-          </button>
-          <button className="flex flex-col items-center justify-center">
-            <li className="h-full w-full flex items-center justify-center">
-              <HiOutlineMail className="text-[1.6rem] lg:text-[2rem] text-[#7d1f2e]" />
-              <BubbleCount popUpSection={"All"} topBarSection="Messages"/>
-            </li>
-          </button>
-        </button>
-        <button
-          type="button"
-          onClick={() => togglePopUp(2)}
-          className="relative flex items-center justify-center"
-        >
-          <button className="absolute right-6 h-auto w-full flex items-center justify-end ">
-            <GoTriangleDown
-              className={`text-2xl text-[#7d1f2e] ${
-                isPopUpOpen2 ? "flex" : "hidden"
-              }`}
-            />
-          </button>
-          <button className="flex flex-col items-center justify-center">
-            <li className="h-full w-full flex items-center justify-center">
-              <HiOutlineBell className="text-[1.6rem] lg:text-[1.9rem] text-[#7d1f2e]" />
-              <BubbleCount popUpSection={"All"} topBarSection="Notifications"/>
-            </li>
-          </button>
-        </button>
-
-        <button
-          type="button"
-          onClick={() => togglePopUp(3)}
-          className="relative flex items-center justify-center"
-        >
           <button className="absolute right-36 h-auto w-full flex items-center justify-end ">
             <GoTriangleDown
               className={`text-2xl text-[#7d1f2e] ${
-                isPopUpOpen3 ? "flex" : "hidden"
+                isPopUpOpen1 ? "flex" : "hidden"
               }`}
             />
           </button>
